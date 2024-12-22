9a2bfe8683304f3cf787bfaea209b52cd97bbad1
HPNV <111484012+HPNV@users.noreply.github.com>
2024-12-22 21:09:38 +0700

    tes

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,25 @@
 
 use App\Http\Controllers\HotelController;
 use App\Http\Controllers\PlaceController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PlaceController::class, 'index']);
+// Public routes
+Route::get('/', [PlaceController::class, 'index'])->name('home');
 Route::get('places/{placeId}', [PlaceController::class, 'show'])->name('places.show');
-Route::get('places/{placeId}/book', [PlaceController::class, 'showBook'])->name('places.book');
-Route::post('places/{placeId}/book', [PlaceController::class, 'book'])->name('places.book.submit');
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+
+// Authenticated routes
+Route::middleware('auth')->group(function () {
+    Route::get('profile', [AuthController::class, 'showProfile'])->name('profile');
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('places/{placeId}/book', [PlaceController::class, 'showBook'])->name('places.book');
+    Route::post('places/{placeId}/book', [PlaceController::class, 'book'])->name('places.book.submit');
+});
+
+// API routes
 Route::get('api/hotels/{hotelId}/rooms', [HotelController::class, 'getRooms']);
