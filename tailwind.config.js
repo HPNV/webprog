901d162aba17efb7ffe6f96f2684397f3307e561
Josiah Owen Yoswandi <josiah.yoswandi@gmail.com>
2024-12-28 14:43:23 +0700

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
+    "./Resources/**/*.blade.php",
+    "./Resources/**/*.js",
+    "./Resources/**/*.vue",
   ],
   theme: {
     extend: {
