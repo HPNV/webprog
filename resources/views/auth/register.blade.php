b4d61227c40e41d84560e44feba7bdc7af2343c7
Josiah Owen Yoswandi <josiah.yoswandi@gmail.com>
2024-12-28 15:01:37 +0700

    update http to https

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -46,13 +46,13 @@
 
                         <div class="flex mt-8 justify-center w-full">
                             <button class="mr-5 bg-blue-700 border-none px-6 py-3 rounded-xl cursor-pointer text-white shadow-xl hover:shadow-inner transition duration-500 ease-in-out transform hover:-translate-x hover:scale-105">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                                <svg xmlns="https://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                                     <path d="M12 2.04C6.48 2.04 2 6.52 2 12c0 4.93 3.65 9.06 8.44 9.88v-7h-2.5v-2.88h2.5v-2.2c0-3.02 1.79-4.7 4.51-4.7 1.31 0 2.69.12 3.03.18v3h-2.13c-1.67 0-2.09.81-2.09 1.99v2.63h4.16l-.67 2.88h-3.5v7c4.79-.82 8.44-4.95 8.44-9.88 0-5.48-4.48-9.96-10-9.96z"/>
                                 </svg>
                             </button>
 
                             <button class="bg-blue-500 border-none px-6 py-3 rounded-xl cursor-pointer text-white shadow-xl hover:shadow-inner transition duration-500 ease-in-out transform hover:-translate-x hover:scale-105">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" fill="currentColor" class="w-6 h-6">
+                                <svg xmlns="https://www.w3.org/2000/svg" viewBox="0 0 48 48" fill="currentColor" class="w-6 h-6">
                                     <path fill="#FFC107" d="M43.611 20.083H42V20H24v8h11.303c-1.649 4.657-6.08 8-11.303 8c-6.627 0-12-5.373-12-12 c0-6.627 5.373-12 12-12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4C12.955 4 4 12.955 4 24 c0 11.045 8.955 20 20 20c11.045 0 20-8.955 20-20C44 22.659 43.862 21.35 43.611 20.083z"/>
                                     <path fill="#FF3D00" d="M6.306 14.691l6.571 4.819C14.655 15.108 18.961 12 24 12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657 C34.046 6.053 29.268 4 24 4C16.318 4 9.656 8.337 6.306 14.691z"/>
                                     <path fill="#4CAF50" d="M24 44c5.166 0 9.86-1.977 13.409-5.192l-6.19-5.238C29.211 35.091 26.715 36 24 36 c-5.202 0-9.619-3.317-11.283-7.946l-6.522 5.025C9.505 39.556 16.227 44 24 44z"/>
--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -29,7 +29,7 @@
         <div id="imageModal" class="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 hidden z-50">
             <div class="relative max-w-xl bg-white rounded-lg p-6">
                 <button type="button" class="absolute top-2 right-2 rounded-full bg-white text-gray-500 p-2" onclick="closeModal()">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <svg xmlns="https://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
                     </svg>
                 </button>
